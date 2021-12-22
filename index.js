6966c3701e98c5a726411365b3f0a1024d351d59
Nurudeeen <nurudeenrabiu81@gmail.com>
2021-12-22 13:04:05 +0100

    added a place feature

--- index.js
+++ index.js
@@ -66,7 +66,54 @@ distance.matrix(origins, destinations, function (err, distances) {
     }
 });
 });
-
+app.post('/place',(request,response)=>{
+    //posts location data from front-end or esp 32 postb request
+    data=request.body    
+    // console.log(data);
+    origins = []
+    origins[0]= data.place
+    console.log(origins)
+    bus = data.bus
+    var destinations = ['7.519554708957414, 4.5210832268552394'];
+    
+    distance.matrix(origins, destinations, function (err, distances) {
+        if (err) {
+            return console.log(err);
+        }
+        if(!distances) {
+            return console.log('no distances');
+        }
+        if (distances.status == 'OK') {
+                    var origin = distances.origin_addresses;
+                    var destination = distances.destination_addresses;
+                    if (distances.rows[0].elements[0].status == 'OK') {
+                        var time = distances.rows[0].elements[0].duration.text;
+                        var distance = distances.rows[0].elements[0].distance.text;
+                        console.log('The '+bus+' bus will arrive in '+time);
+                        console.log(bus+' bus'+ ' is ' +distance+ ' away');
+                        now = new Date ()
+                        var info = {bus, distance, time, now}
+                        response.json({
+                            status: 'Server Connected to Client',
+                                distance: info.distance,
+                                time: info.time
+                        })
+                        
+                        db.insert(info)
+                        
+                        // db.update({bus: 'Ikeja'}, {distance: distance, time: time}, {}, function(err, numReplaced){
+                        //     if(err) console.log(err);
+                            
+                        //   });
+                    } else {
+                        console.log(destination + ' is too far from ' + origin);
+                        response.json(destination + ' is too far from ' + origin)
+                    }
+                
+            
+        }
+    });
+    });
 app.get('/api', (request,response)=>{
    //gets latest input into database from post request
     
